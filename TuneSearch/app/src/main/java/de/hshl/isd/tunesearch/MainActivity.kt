9f6172475e63f32b81b11fce3832889bcae6d8eb
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-01-09 14:36:37 +0100

    Licenses with license plugin

--- TuneSearch/app/src/main/java/de/hshl/isd/tunesearch/MainActivity.kt
+++ TuneSearch/app/src/main/java/de/hshl/isd/tunesearch/MainActivity.kt
@@ -2,13 +2,11 @@ package de.hshl.isd.tunesearch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
-import androidx.navigation.findNavController
-import de.hshl.isd.tunesearch.common.InputBoundary
-import de.hshl.isd.tunesearch.common.OutputBoundary
-import de.hshl.isd.tunesearch.common.Response
-import de.hshl.isd.tunesearch.ui.main.MainFragment
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         val id = item.itemId
 
         when (id) {
-            R.id.action_info -> LicenseDialogFragment().show(supportFragmentManager, "Dialog")
+            R.id.action_info -> startActivity(Intent(this, OssLicensesMenuActivity::class.java));
         }
 
 
